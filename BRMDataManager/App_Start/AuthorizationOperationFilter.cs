19cb809ee7f64affd2df98c359332298e0751d3a
Dhiraj Kumar <DKumar@paycor.com>
2020-08-05 22:08:01 +0530

    Add swagger documentation to the API
    Added swashBuckle/swagger to the API for documentation and testing

---
+++ BRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,34 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace BRMDataManager.App_Start
+{
+	public class AuthorizationOperationFilter : IOperationFilter
+    {
+        /// <summary>
+        /// Applies the operation filter.
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="schemaRegistry"></param>
+        /// <param name="apiDescription"></param>
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
