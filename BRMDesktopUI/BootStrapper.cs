d08bd854bb6be3a9f238db5808409bab93212547
Dhiraj Kumar <DKumar@paycor.com>
2020-08-11 16:55:43 +0530

    Add MVVM WPF project
    Added a window project(WPF) and introduced the basic structure for the project to initiate runtime with a custom class BootStrapper

---
+++ BRMDesktopUI/BootStrapper.cs
@@ -0,0 +1,23 @@
+﻿using BRMDesktopUI.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BRMDesktopUI
+{
+	public class BootStrapper : BootstrapperBase
+	{
+		public BootStrapper()
+		{
+			Initialize();
+		}
+		protected override void OnStartup(object sender, StartupEventArgs e)
+		{
+			DisplayRootViewFor<ShellViewModel>();
+		}
+	}
+}
