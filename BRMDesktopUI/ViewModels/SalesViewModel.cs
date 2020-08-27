1f3708dd2d6b055f0e1605fa2d2cfabb4b097586
Dhiraj Kumar <DKumar@paycor.com>
2020-08-27 23:38:06 +0530

    Simplify the Salesview tax calculation foreach to LINQ

--- BRMDesktopUI/ViewModels/SalesViewModel.cs
+++ BRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -103,15 +103,19 @@ namespace BRMDesktopUI.ViewModels
 		private decimal CalculateTax()
 		{
 			decimal taxAmount = 0;
-			decimal taxRate = _configHelper.GetTaxRate();
+			decimal taxRate = _configHelper.GetTaxRate() / 100;
 
-			foreach (var item in Cart)
-			{
-				if (item.Product.IsTaxable)
-				{
-					taxAmount += (item.Product.RetailPrice * item.QuantityInCart * (taxRate / 100));
-				}
-			}
+			taxAmount = Cart
+				.Where(x => x.Product.IsTaxable)
+				.Sum(y => y.Product.RetailPrice * y.QuantityInCart * taxRate);
+
+			//foreach (var item in Cart)
+			//{
+			//	if (item.Product.IsTaxable)
+			//	{
+			//		taxAmount += (item.Product.RetailPrice * item.QuantityInCart * (taxRate));
+			//	}
+			//}
 
 			return taxAmount;
 		}
